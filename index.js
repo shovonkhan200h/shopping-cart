db999aaaa7fceebf2d279e4d95dcbc3d81a73069
shovonkhan200h <shovonkhan00@gmail.com>
2022-11-02 16:54:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const add=()=>{
+        document.getElementById(`plusBtn`).addEventListener(`click`,()=>{
+        const valueNumber =setSpan(`count`)
+        const result= document.getElementById(`count`).innerText = valueNumber + 1;
+        updatePrice(`price`,result);
+    })
+    
+}
+
+const miuns=()=>{
+        let minusButton= document.getElementById(`minusBtn`);
+        minusButton.addEventListener(`click`,()=>{
+        const valueNumber =setSpan(`count`)
+        const result= document.getElementById(`count`).innerText = valueNumber - 1;
+        updatePrice(`price`,result);
+        })
+        
+}
+
+const updatePrice = (id,result)=>{
+    const price= document.getElementById(id).innerText;
+    let priceNumber= +price;
+    priceNumber=1219;
+    const total= result * priceNumber;
+    document.getElementById(id).innerText= total;
+}
+
+const setSpan = (id)=>{
+    const value= document.getElementById(id).innerText;
+    const valueNumber= +value;
+    return valueNumber; 
+};
+
+add()
+miuns()
